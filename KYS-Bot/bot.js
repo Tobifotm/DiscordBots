aabf94c4e20c623037176ce90049d33686b46f41
Tobias Grothusmann <tobias.grothusmann@xcom.de>
2017-07-20 17:57:14 +0200

    Added Initial KYS-Bot

---
+++ KYS-Bot/bot.js
@@ -0,0 +1,13 @@
+var Discordie = require("discordie");
+var client = new Discordie();
+
+client.connect({ token: "" });
+
+client.Dispatcher.on("GATEWAY_READY", e => {
+  console.log("Connected as: " + client.User.username);
+});
+
+client.Dispatcher.on("MESSAGE_CREATE", e => {
+  if (e.message.content == "ping")
+    e.message.channel.sendMessage("pong");
+});
